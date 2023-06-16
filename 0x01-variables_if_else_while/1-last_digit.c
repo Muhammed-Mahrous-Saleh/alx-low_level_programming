884ca7b4e44beef3463b4fb566e46f686bfe7862
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-06-16 12:39:37 -0700

    solving tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d ", n, n % 10);
 		printf("and is greater than 5\n");
-	} else if (n == 0)
+	} else if (n % 10 == 0)
 	{
 		printf("Last digit of %d is %d ", n, n % 10);
 		printf("and is 0\n");
