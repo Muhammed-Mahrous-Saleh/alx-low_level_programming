ef9e4f8dd4d12ba0a1e3935ea4218e11db627b91
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-06-20 10:58:52 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "_putchar.c"
+
+
+/*
+ * main: the main task,
+ * Return: always return 0 (Success)
+ */
+int main(void)
+{
+	char outMes[] = "_putchar\n";
+	for (int i = 0; i < 9; i++)
+	{
+		_putchar(outMes[i]);
+	}
+	return (0);
+}
